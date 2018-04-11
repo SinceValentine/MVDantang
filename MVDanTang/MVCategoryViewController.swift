6a4c3e7b2f15d2cbfc2e13bcde0df74eb973d218
shanmm <shanshao21@163.com>
2018-04-11 22:42:22 +0800

    2018

--- MVDanTang/MVCategoryViewController.swift
+++ MVDanTang/MVCategoryViewController.swift
@@ -13,7 +13,7 @@ class MVCategoryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // test for upload
+        // test for upload 20182018
         // Do any additional setup after loading the view.
     }
 
